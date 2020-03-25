a3aff6ebd712f7be7efc9213829c2c13ba7fc0a7
Pratham Singh <prathms007@gmail.com>
2020-03-25 18:52:31 +0530

    handling secret from json file

---
+++ backend/secret_handler.py
@@ -0,0 +1,26 @@
+import json
+import crypt_lib
+
+
+class SecretHandler():
+
+    def __init__(self, key):
+        self.data = {}
+        self.crypter = crypt_lib.AESCipher(key)
+        self.read_file()
+
+    def read_file(self):
+        with open('enc.json') as f:
+            self.data = json.load(f)
+
+    def write_data(self, new_data):
+        site = new_data['site'].lower()
+        new_data['password'] = self.crypter.encrypt(new_data['password'])
+        self.data['pass'][site] = new_data
+
+        with open('enc.json', 'w') as f:
+            json.dump(self.data, f)
+
+    def get_password(self, site):
+        password = self.data['pass'][site]['password']
+        return self.crypter.decrypt(password)
