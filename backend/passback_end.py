f22d707cb1df6bbb60e6d017dfd832c6ea372b78
Pratham Singh <prathms007@gmail.com>
2020-03-28 19:57:13 +0530

    converting logger to a method

--- backend/passback_end.py
+++ backend/passback_end.py
@@ -3,11 +3,6 @@ from app_drive import AppDrive
 from secret_handler import SecretHandler
 
 
-def logger(msg):
-    func_name = sys._getframe(1).f_code.co_name
-    print('[ %s ]: %s.', func_name, msg)
-
-
 class Backend():
     def __init__(self):
         self.gd = None
@@ -17,12 +12,16 @@ class Backend():
         self.gd = AppDrive()
         is_logged = self.gd.is_loggedIn()
         if is_logged is None:
-            logger('Cannot Login')
+            self._logger('Cannot Login')
             return False
-        logger('Logged In')
+        self._logger('Logged In')
         return True
 
     def get_data(self, key):
         self.sh = SecretHandler(key)
         self.sh.read_file()
         return self.data
+
+    def _logger(self, msg):
+        func_name = sys._getframe(1).f_code.co_name
+        print('[ %s ]: %s.', func_name, msg)
