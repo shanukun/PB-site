1cdf1c06ef8f34dd441756d1e91b02355a0c138d
Pratham Singh <prathms007@gmail.com>
2020-03-29 19:26:04 +0530

    login screen process

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route, Switch, Link, Redirect } from 'react-router-dom';
 import './App.css';
 
+const API = "http://localhost:5000/api/"
 
 function App() {
 	const [isLogged, setLogged] = useState(false);
@@ -19,20 +20,59 @@ function App() {
 }
 
 function LoginScreen(props) {
+	const [showMsg, setShowMsg] = useState(false);
+	const [msg, setMsg] = useState('');
+
+	const getJsonData = async () => {
+		const resp = await fetch(API + "gdrivelogin");
+		resp.json().then(data => {
+			if (data["login"] === true) {
+				props.setLogged(true)
+				setShowMsg(false);
+				props.history.push('/password')
+			} else {
+				setMsg('Error.');
+			}
+		});
+	}
+
+	const handleClick = (e) => {
+		e.preventDefault()
+		setMsg('Wait...');
+		setShowMsg(true);
+		getJsonData()
+	}
+
 	return (
 		<div>
-			<button className='login'>Login with Google</button>
+			<button className='login' onClick={handleClick}>Login with Google</button>
+			{ showMsg ? <div className='msg'>{msg}</div> : null }
 		</div>
 	);
 }
 
 function PasswordScreen() {
+	const [password, setPassword] = useState('');
+
+	const handleClick = (e) => {
+		e.preventDefault();
+	}
+
 	return (
 		<div>
-			<label for="inputPassword">Password:</label>
-			<input type="password" className="keyPassword" id="inputPassword" />
+			<label>Password:</label><br />
+			<input type="password" className="keyPassword" id="inputPassword" /><br />
+			<button onClick={handleClick}>Enter</button>
 		</div>
 
+	);
+}
+
+function Dashboard() {
+	return (
+		<div>
+			<h3>Dashboard</h3>
+		</div>
 	);
 }
 
@@ -40,7 +80,7 @@ function Unauthorized()  {
 	return (
 		<div>
 			<h3>403 - Unauthorized</h3>
-			<p><Link to="/unauth" /></p>
+			<p><Link to="/">Back To Home</Link></p>
 		</div>
 	);
 }
