ce79d4891ea426acac9898a9d8b256295432354b
Pratham Singh <prathms007@gmail.com>
2020-03-29 18:47:26 +0530

    added gdrivelogin route and cors

--- backend/handler.py
+++ backend/handler.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from passback_end import Backend
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
+be = Backend()
 
 
-@app.route("/gdrivelogin")
+@app.route("/api/gdrivelogin")
 def gdrive_login():
-    pass
+    if (be.gdrive_login()):
+        return jsonify(login=True)
+    return jsonify(login=False)
 
 
 if __name__ == "__main__":
