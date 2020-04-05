4473e8ad2aad9b85b71e5ddd22309f6d62029314
Pratham Singh <prathms007@gmail.com>
2020-04-05 22:40:40 +0530

    taking key into account

--- backend/service.go
+++ backend/service.go
@@ -5,7 +5,7 @@ import "log"
 // Service interface for handler
 type Service interface {
 	GetAll() []Credential
-	GetPass(crypt Crypter) string
+	GetPass(cred Credential) string
 	EnterNew(cred Credential) error
 	close()
 }
@@ -20,14 +20,17 @@ func (s *ServiceImpl) GetAll() []Credential {
 	return creds
 }
 
-func (s *ServiceImpl) GetPass(key string, cred Credential) string {
+func (s *ServiceImpl) GetPass(cred Credential) string {
 	pass := s.credRepo.FindPass(cred)
-	c := &Crypter{key, pass}
+	c := &Crypter{cred.Key, pass}
 	pass, _ = c.Decrypt()
 	return pass
 }
 
 func (s *ServiceImpl) EnterNew(cred Credential) error {
+	c := &Crypter{cred.Key, cred.Password}
+	cred.Password, _ = c.Encrypt()
+
 	err := s.credRepo.InsertNew(cred)
 	if err != nil {
 		log.Fatal(err)
