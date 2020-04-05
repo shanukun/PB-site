94f3633eb9305e9596e485c99b5196123161fa28
Pratham Singh <prathms007@gmail.com>
2020-04-05 22:52:46 +0530

    start pt

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,25 @@
 package main
 
+import "net/http"
+
+func prepareWebService() *ServiceImpl {
+	var (
+		credRepo CredRepository
+
+		sqlitedb *SqliteRepository
+	)
+
+	sqlitedb = NewSqliteRepository("Creds")
+	credRepo = sqlitedb
+
+	service := &ServiceImpl{
+		credRepo: credRepo,
+	}
+	return service
+}
+
 func main() {
-
-	passRepository := NewSqliteRepository()
-
-	service := &ServiceImpl{passRepository}
-
+	service := prepareWebService()
+	router := NewRouter(service)
+	http.ListenAndServe(":8000", router)
 }
