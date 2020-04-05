41124e49e1e233ecfe593eaaff31ec42d59c2b1b
Pratham Singh <prathms007@gmail.com>
2020-04-05 22:52:21 +0530

    handler impl

--- backend/handler.go
+++ backend/handler.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -8,8 +10,13 @@ import (
 
 var service Service
 
+var _get = 0
+var _post = 1
+
 // NewRouter instantiate new router
 func NewRouter(_service Service) *mux.Router {
+	service = _service
+
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/api/", landing)
@@ -19,13 +26,41 @@ func NewRouter(_service Service) *mux.Router {
 }
 
 func landing(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func newEntry(w http.ResponseWriter, r *http.Request) {
-
+	setHeader(&w, _get)
+	creds := service.GetAll()
+	json.NewEncoder(w).Encode(creds)
 }
 
 func getPass(w http.ResponseWriter, r *http.Request) {
+	setHeader(&w, _get)
+	var cred Credential
+	json.NewDecoder(r.Body).Decode(&cred)
+	cred.Password = service.GetPass(cred)
+	json.NewEncoder(w).Encode(cred)
+}
 
+func newEntry(w http.ResponseWriter, r *http.Request) {
+	setHeader(&w, _post)
+	var cred Credential
+	json.NewDecoder(r.Body).Decode(&cred)
+	err := service.EnterNew(cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(struct{ done bool }{done: true})
+}
+
+func setHeader(w *http.ResponseWriter, method int) {
+	(*w).Header().Set("Content-Type", "applications/x-www-form-urlencoded")
+	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	switch method {
+	case _get:
+		(*w).Header().Set("Access-Control-Allow-Methods", "POST")
+		break
+	case _post:
+		(*w).Header().Set("Access-Control-Allow-Methods", "GET")
+		break
+	}
 }
