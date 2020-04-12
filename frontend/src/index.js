aaa0e3712293493797bf0cf5eb78dd40d0caad35
Pratham Singh <prathms007@gmail.com>
2020-04-12 15:09:26 +0530

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import StoreProvider from './Store';
+import { StoreProvider } from './Store';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
