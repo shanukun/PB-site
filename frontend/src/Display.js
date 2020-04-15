a34b535c505178e93e8ecd46337ff4c2803d1be8
Pratham Singh <prathms007@gmail.com>
2020-04-15 18:08:45 +0530

    toggle pass list function

--- frontend/src/Display.js
+++ frontend/src/Display.js
@@ -7,7 +7,7 @@ const ShowBtn = (props) => {
 	const [show, setShow] = React.useState(false);
 	const handleClick = () => {
 		setShow(!show);
-		props.fetchPass(props.id)
+		props.togglePass(props.id);
 	}
 	return (
 		<button type="button" className="btn btn-sm btn-light" onClick={handleClick}>
@@ -20,13 +20,26 @@ function Display(props) {
 	const { state } = React.useContext(Store);
 	const [passState, setPassState] = React.useState({});
 
-	const addPassToLIst = (id, pass) => {
-		const passObject = {pass: pass, show: true};
+	const togglePass = (id) => {
+		if (passState.hasOwnProperty(id)) {
+			if (passState[id]['show'] === true) {
+				changePassList(id, false);
+			} else {
+				changePassList(id, true);
+			}
+		} else {
+			fetchPassword(id);
+		}
+	}
+
+	const changePassList = (id, showState) => {
 		setPassState(passState => ({
 			...passState,
-			[id]: passObject
+			[id]: {
+				...passState[id],
+				show: showState
+			}
 		}));
-
 	}
 
 	const fetchPassword = async (id) => {
@@ -36,7 +49,15 @@ function Display(props) {
 			"cred_id": id})
 		});
 		const dataJson = await data.json();
-		addPassToLIst(id, dataJson.password);
+		addPassToList(id, dataJson.password);
+	}
+
+	const addPassToList = (id, pass) => {
+		const passObject = {pass: pass, show: true};
+		setPassState(passState => ({
+			...passState,
+			[id]: passObject
+		}));
 	}
 
 	return (
@@ -48,10 +69,17 @@ function Display(props) {
 							<div className="row">
 								<div className="col-2"><strong>{cred.domain}</strong></div>
 								<div className="col-3">{cred.username}</div>
-								<div className="col-sm">{ cred.password }</div>
-								{/* <div className="col-sm">{ passState[cred.cred_id]['show'] ? passState[cred.cred_id]['pass'] : cred.password }</div> */}
+								{/* <div className="col-sm">{ cred.password }</div> */}
+								<div className="col-sm">
+									{ passState.hasOwnProperty(cred.cred_id) ? 
+											passState[cred.cred_id]['show'] === true ? 
+												passState[cred.cred_id]['pass'] : 
+												cred.password : 
+											cred.password 
+									}
+								</div>
 								<div className="col-md-auto">
-									<ShowBtn id={cred.cred_id} fetchPass={fetchPassword} />
+									<ShowBtn id={cred.cred_id} togglePass={togglePass} />
 								</div>
 								<div className="col-md-auto pl-0">
 									<button type="button" className="btn btn-sm btn-light">
