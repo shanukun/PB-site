ff4496160c14a3186af3c2269561bbc4fe33f562
Pratham Singh <prathms007@gmail.com>
2020-04-15 16:24:00 +0530

    putting showbtn outside component, adding list

--- frontend/src/Display.js
+++ frontend/src/Display.js
@@ -3,8 +3,31 @@ import { Store } from './Store';
 
 const API = "http://localhost:8000/api/";
 
+const ShowBtn = (props) => {
+	const [show, setShow] = React.useState(false);
+	const handleClick = () => {
+		setShow(!show);
+		props.fetchPass(props.id)
+	}
+	return (
+		<button type="button" className="btn btn-sm btn-light" onClick={handleClick}>
+			{ show ? "Hide" : "Show" }
+		</button>
+	);
+}
+
 function Display(props) {
 	const { state } = React.useContext(Store);
+	const [passState, setPassState] = React.useState({});
+
+	const addPassToLIst = (id, pass) => {
+		const passObject = {pass: pass, show: true};
+		setPassState(passState => ({
+			...passState,
+			[id]: passObject
+		}));
+
+	}
 
 	const fetchPassword = async (id) => {
 		const data = await fetch(API + "getpass", {
@@ -13,22 +36,7 @@ function Display(props) {
 			"cred_id": id})
 		});
 		const dataJson = await data.json();
-		console.log(dataJson);
-	}
-
-	const ShowBtn = (props) => {
-		const [show, setShow] = React.useState(false);
-
-		const handleClick = () => {
-			setShow(!show);
-			fetchPassword(props.id)
-		}
-
-		return (
-			<button type="button" className="btn btn-sm btn-light" onClick={handleClick}>
-				{ show ? "Hide" : "Show" }
-			</button>
-		);
+		addPassToLIst(id, dataJson.password);
 	}
 
 	return (
@@ -37,12 +45,13 @@ function Display(props) {
 				return (
 					<div className="card text-white bg-dark mb-3">
 						<div className="card-body">
-							<div key={cred.cred_id} className="row">
+							<div className="row">
 								<div className="col-2"><strong>{cred.domain}</strong></div>
 								<div className="col-3">{cred.username}</div>
-								<div className="col-sm">{cred.password}</div>
+								<div className="col-sm">{ cred.password }</div>
+								{/* <div className="col-sm">{ passState[cred.cred_id]['show'] ? passState[cred.cred_id]['pass'] : cred.password }</div> */}
 								<div className="col-md-auto">
-									<ShowBtn id={cred.cred_id} />
+									<ShowBtn id={cred.cred_id} fetchPass={fetchPassword} />
 								</div>
 								<div className="col-md-auto pl-0">
 									<button type="button" className="btn btn-sm btn-light">
